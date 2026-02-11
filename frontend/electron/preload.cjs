f73d52efbedbb7167f8b812751246fc8b2adb1ef
w-electro <wforelectronicservices@gmail.com>
2026-02-11 19:16:52 +0300

    DELETE

--- frontend/electron/preload.cjs
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  platform: process.platform,
-  minimize: () => ipcRenderer.send('window:minimize'),
-  maximize: () => ipcRenderer.send('window:maximize'),
-  close: () => ipcRenderer.send('window:close'),
-});
